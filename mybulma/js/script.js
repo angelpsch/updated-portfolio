6ebde07a3dedc4915565d2c281ae158b387031f0
angelpsch <angelpsch@users.noreply.github.com>
2020-05-06 13:35:34 -0500

    attempt on github embed

--- mybulma/js/script.js
+++ mybulma/js/script.js
@@ -8,17 +8,42 @@ $(document).ready(function() {
         $(".navbar-menu").toggleClass("is-active");
   
     });
- 
-    $(".panel-tabs").click(function() {
-      var id = $('.panel-block').attr('id');
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        $('.panel-tabs').toggleClass("is-active");
-        $(id).toggleClass('is-invisible'); 
 
-  
-    });
-  });
-
-  $('#cancel').click(function() {
+     $('#cancel').click(function() {
       $('.input').empty(); 
     })
+
+
+    $('.panel-tabs a').click(function(event) {
+
+        event.preventDefault(); 
+        var showThis = $(this).attr('href');
+        $('.panel-tabs a').removeClass('is-active'); 
+        $(this).toggleClass('is-active'); 
+
+
+        $('.panel-block').hide(); 
+        $(showThis).show();
+
+        $('.tab-title').text($(this).text())
+        })
+    
+    $('.dropdown').click(function() {
+        $(this).toggleClass('is-active'); 
+    
+
+    })
+
+    $('#submit').click(function(){
+       $("#contact-info").empty(); 
+        $('#contact-form').html('<h1 class="title">Message sent! Expect a response within 48 business hours.</h1><p class="help">If you need a response sooner, please send me a<a href="mailto:angel.p.schultz@gmail.com"> direct email</a> or <a href="tel:3198304230">call me</a></p>'); 
+    });
+ 
+
+  });
+
+
+  //4d023697b930daeb8499ec6bf617e602ca6f8a0d 
+  //personal access token 
+
+ 
